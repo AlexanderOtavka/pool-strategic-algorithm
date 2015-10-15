504df8ec70b76b8695538ac830d2d997e50bc798
Zander Otavka <zotavka@gmail.com>
2015-10-15 15:36:50 -0700

    Restructure PortManager to work with async xbee.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from __future__ import division
 import json
 
 from pyglet.window import Window
-from pyglet.clock import schedule_interval
 from pyglet.app import run, event_loop
 from pyglet.gl import glClearColor
 
@@ -22,9 +21,9 @@ from render import PrimitiveRenderer, batch
 __author__ = "Zander Otavka"
 
 
-with open("port.json", "r") as f:
+with open("config.json", "r") as f:
     json_data = json.load(f)
-port_manager = PortManager(json_data["device"], json_data["port"])
+port_manager = PortManager(json_data["port"])
 
 window = Window(1080, 540)
 balls = BallGroup()
@@ -35,14 +34,15 @@ glClearColor(0.2, 0.6, 0.3, 1)
 @window.event
 def on_draw():
     window.clear()
-    PrimitiveRenderer.update_all_vertex_lists()
     batch.draw()
 
 
 @port_manager.event
 def on_get_data(data):
     balls.update(data)
-    port_manager.send_shot_data(Shot(angle=0, elevation=0, force=10))
+    shot = Shot(angle=0, elevation=0, force=10)
+    PrimitiveRenderer.update_all_vertex_lists()
+    return shot.serialize()
 
 
 @event_loop.event
@@ -50,5 +50,5 @@ def on_exit():
     port_manager.close()
 
 
-schedule_interval(lambda dt: port_manager.listen(), 1/60)
+port_manager.open()
 run()
--- portmanager.py
+++ portmanager.py
@@ -6,60 +6,56 @@ from xbee import XBee
 __author__ = "Zander Otavka"
 
 
+FAKE_DATA = [
+    200, 100,
+    400, 300,
+    600, 200,
+    800, 400,
+    230, 450,
+    120, 300,
+    110, 130,
+    345, 139,
+    824, 460,
+    200, 200,
+    400, 250,
+    600, 100,
+    800, 204,
+    230, 250,
+    120, 200,
+    110, 230,
+]
+
+
 class PortManager(object):
 
-    _serial = None
+    _serial_port = None
     _xbee = None
     _on_get_data = None
-    _old_data = None
 
-    def __init__(self, device, port):
-        # ser = Serial(device, port)
-        self._serial = Serial()
-        self._xbee = XBee(self._serial)
+    def __init__(self, port):
+        # self._serial_port = Serial(port, 9600)
+        print "open port: {}".format(port)
+        self._serial_port = Serial()
 
-    def _get_data(self):
-        data = [
-            200, 100,
-            400, 300,
-            600, 200,
-            800, 400,
-            230, 450,
-            120, 300,
-            110, 130,
-            345, 139,
-            824, 460,
-            200, 200,
-            400, 250,
-            600, 100,
-            800, 204,
-            230, 250,
-            120, 200,
-            110, 230,
-        ]
-        if data == self._old_data:
-            return None
-        else:
-            print "load data from xbee: {}".format(self._xbee)
-            self._old_data = data
-            return data
-
-    def send_shot_data(self, shot):
-        print "send data to xbee: {}".format(self._xbee)
-        print shot.serialize()
+    def _send_data(self, data):
+        # TODO: implement PortManager._send_data
+        print "send data: {} to xbee: {}".format(data, self._xbee)
 
     def event(self, func):
         if func.__name__ == "on_get_data":
-            self._on_get_data = func
-        return func
+            def on_get_data(data):
+                # TODO: parse the data into an array
+                self._send_data(func(data))
+            self._on_get_data = on_get_data
+            return on_get_data
 
-    def listen(self):
+    def open(self):
         assert self._on_get_data is not None
 
-        data = self._get_data()
-        if data is not None:
-            self._on_get_data(data)
+        # self._xbee = XBee(self._serial_port, callback=self._on_get_data)
+        self._on_get_data(FAKE_DATA)
 
     def close(self):
-        print "closing port {}".format(self._serial)
-        self._serial.close()
+        print "closing port {}".format(self._serial_port)
+        self._xbee.halt()
+        self._serial_port.close()
